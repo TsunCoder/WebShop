f3292f774c269ed9eb9af5ec217f26b2a316ef0e
Thiên Sơn <62060856+TsunCoder@users.noreply.github.com>
2022-03-23 09:35:52 +0700

    23/3

---
+++ mywebsite/src/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Register.css'
+function Register() {
+  return (
+    <div class="header">
+      <h1>Đăng ký</h1>
+      <img src='https://icon2.cleanpng.com/20180421/viq/kisspng-diamond-cut-jewellery-computer-icons-5adb98b65a80f8.7747777915243409183707.jpg' alt=''/>
+    </div>
+  )
+}
+
+export default Register
